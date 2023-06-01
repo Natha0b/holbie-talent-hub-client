561b88d64a165ec048877d4abf762848d2036a2c
YuranyUlchur <ulchuryurani80@gmail.com>
2023-06-01 15:02:48 -0500

    add changes an file of components

--- src/app/(company)/(pages)/find/profile/page.tsx
+++ src/app/(company)/(pages)/find/profile/page.tsx
@@ -8,25 +8,21 @@ import { Back_card } from '../../../../components/card/card_back'
 import { Job_card } from '../../../../components/card/card_job'
 import { Submit_button } from '../../../../components/card/button_submit'
 
-export default function Find(){
+export default function Find() {
     return (
         <section className="profile">
             <h1 className="profile__title">Select the profile that you want</h1>
             <nav className="profile__navigation">
-                {
-                    profiles.map(({jobName, icon: Icon, location, kindJob}) => (
-                        <article className="card">
-                            <Front_card />
-                            <aside  className="card__back">
-                                <form className='form' action="edit-profile" method="post">
-                                    <Back_card />
-                                    <Job_card />                                    
-                                    <Submit_button />
-                                </form>
-                            </aside>
-                        </article>
-                    ))
-                }
+                <article className="card">
+                    <Front_card />
+                    <aside className="card__back">
+                        <form className='form' action="edit-profile" method="post">
+                            <Back_card />
+                            <Job_card />
+                            <Submit_button />
+                        </form>
+                    </aside>
+                </article>
             </nav>
         </section>
     )
--- src/app/components/card/card_front.tsx
+++ src/app/components/card/card_front.tsx
@@ -4,16 +4,18 @@ import { DiAndroid, DiAptana, DiDatabase } from "react-icons/di";
 
 const Front_card = () => {
     return(
-        <div>
-            <article className="card">
-                <main className="card__front">
-                    <Icon className="card__icon" />
-                    <h4 className="card__title">{jobName}</h4>
-                </main>
-            </article>
-        </div>
+        profiles.map(({jobName, icon: Icon}) => (
+            <div>
+                <article className="card">
+                    <main className="card__front">
+                        <Icon className="card__icon" />
+                        <h4 className="card__title">{jobName}</h4>
+                    </main>
+                </article>
+            </div>
         )
+        )
+    )
 }
 
 export { Front_card };
-
