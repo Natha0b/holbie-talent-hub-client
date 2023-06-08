ef37eff08ad2d88cd5c416ba14775dc830daea39
YuranyUlchur <ulchuryurani80@gmail.com>
2023-06-08 18:19:16 -0500

    add changes in profile of the user and implement to carousel

--- src/app/(company)/(pages)/profiles-holbies/page.tsx
+++ src/app/(company)/(pages)/profiles-holbies/page.tsx
@@ -2,33 +2,55 @@
 import React from 'react';
 import { useState } from "react";
 import './profiles.css';
+import DashboardLayout from '../find/layout';
+import Image from 'next/image';
+import javascriptIcon from '../../../../../public/imagenescoderise/java-script1.png';
+import cIcon from '../../../../../public/imagenescoderise/c-.png';
+import rubyIcon from '../../../../../public/imagenescoderise/ruby.png';
 
 interface User {
     name: string;
     profilePicture: string;
     age: number;
     programmingLanguages: string[];
+    githubUrl: string;
+    linkedinUrl: string;
+    gmailUrl: string;
+    slackUrl: string;
+
 }
 
 const HomeProfiles: React.FC = () => {
     const users: User[] = [
         {
-            name: 'John Doe Holbie Random',
-            profilePicture: '/profile1.jpg',
+            name: 'John Doe',
+            profilePicture: 'profile1.jpg',
             age: 30,
-            programmingLanguages: ['JavaScript', 'Python', 'C', 'C++'],
-        },
-        {
-            name: 'Jane Smith',
-            profilePicture: '/profile2.jpg',
-            age: 25,
-            programmingLanguages: ['Java', 'C#', 'Ruby'],
+            programmingLanguages: ['JavaScript', 'C'],
+            githubUrl: 'https://github.com/',
+            linkedinUrl: 'https://linkedin.com/',
+            gmailUrl: 'https://gmail.com',
+            slackUrl: 'https://slack.com/',
         },
         {
             name: 'Ana diaz',
-            profilePicture: '/profile2.jpg',
+            profilePicture: 'profile2.jpg',
             age: 25,
-            programmingLanguages: ['Java', 'C#', 'Ruby'],
+            programmingLanguages: ['JavaScript', 'Ruby'],
+            githubUrl: 'https://github.com/',
+            linkedinUrl: 'https://linkedin.com/',
+            gmailUrl: 'https://gmail.com',
+            slackUrl: 'https://slack.com/',
+        },
+        {
+            name: 'Jane Smith',
+            profilePicture: 'profile3.jpg',
+            age: 25,
+            programmingLanguages: ['JavaScript', 'C', 'Ruby'],
+            githubUrl: 'https://github.com/',
+            linkedinUrl: 'https://linkedin.com/',
+            gmailUrl: 'https://gmail.com',
+            slackUrl: 'https://slack.com/',
         },
         // Agrega más usuarios aquí si es necesario
     ];
@@ -44,42 +66,78 @@ const HomeProfiles: React.FC = () => {
         setCurrentSlide((prevSlide) => (prevSlide === users.length - 1 ? 0 : prevSlide + 1));
     };
 
+    const getLanguageIcon = (language: string) => {
+        switch (language) {
+            case 'JavaScript':
+                return <Image src={javascriptIcon} alt="JavaScript" width={32} height={32} />;
+            case 'C':
+                return <Image src={cIcon} alt="C" width={32} height={32} />;
+            case 'Ruby':
+                return <Image src={rubyIcon} alt="Ruby" width={32} height={32} />;
+            default:
+                return null;
+        }
+    };
+
+
     return (
-        <section className="profile">
-            <h1 className="profile__title">Profiles</h1>
-            <div className="carousel">
-                <button className="carousel__button carousel__button--prev" onClick={handlePrevSlide}>
-                    Prev
-                </button>
-                <div className="carousel__slide-container">
-                    {users.map((user, index) => (
-                        <div
-                            className={`carousel__slide ${index === currentSlide ? 'carousel__slide--active' : ''}`}
-                            key={index}
-                        >
-                            <img className="profile-picture" src="/logo.png"  width={50} height={50} />
-                            <div className="user-info">
-                                <h1 className="name">{user.name}</h1>
-                                <p className="title">Software Engineer</p>
-                                <p className="description">
-                                    Experiencia en la industria: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget
-                                    condimentum tortor, vitae gravida ipsum. Nullam ut mi vel nisi auctor fermentum. In hac habitasse
-                                    platea dictumst.
-                                </p>
-                                <p className="description">Lenguajes de Programación: {user.programmingLanguages.join(', ')}</p>
-                                <p className="description">
-                                    Tipo de trabajo buscado: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu diam eget
-                                    dui euismod commodo vitae eget augue. Mauris vitae vestibulum quam.
-                                </p>
+        <DashboardLayout>
+            <section className="profile">
+                <h1 className="profile__title">Profiles</h1>
+                <div className="container">
+                    <div className="card">
+                        <div className="carousel">
+                            <button className="carousel__button carousel__button--prev" onClick={handlePrevSlide}>
+                                &lt;
+                            </button>
+                            <div className="carousel__slide-container">
+                                {users.map((user, index) => (
+                                    <div
+                                        className={`carousel__slide ${index === currentSlide ? 'carousel__slide--active' : ''}`}
+                                        key={index}
+                                    >
+
+                                        <div className="user-info">
+                                            <div className="icon-profile">
+                                                <img className="profile-picture" src={`../../../../imagenescoderise/${user.profilePicture}`} />
+                                            </div>
+                                            <div className="user-details">
+                                                <h1 className="name">{user.name}</h1>
+                                                <p className="title">Software Engineer</p>
+                                                <p className="description">
+                                                    Industry experience: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget
+                                                    condimentum tortor, vitae gravida ipsum. Nullam ut mi vel nisi auctor fermentum. In hac habitasse
+                                                    platea dictumst.
+                                                </p>
+                                                <p className="description">Programming Languages</p>
+                                                <div className="programming-languages">
+                                                    {user.programmingLanguages.map((language, index) => (
+                                                        <div className="icons-languajes" key={index}>{getLanguageIcon(language)}</div>
+                                                    ))}
+                                                </div>
+                                                <p className="description">
+                                                    Type of work sought: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu diam eget
+                                                    dui euismod commodo vitae eget augue. Mauris vitae vestibulum quam.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="contact-icons">
+                                            <a href={user.githubUrl} target="_blank"><img className="icon" src="../../../../imagenescoderise/github.png" alt="GitHub" width={32} height={32} /> </a>
+                                            <a href={user.linkedinUrl} target="_blank"><img className="icon" src="../../../../imagenescoderise/linkedin.png" alt="LinkedIn" width={32} height={32} /> </a>
+                                            <a href={user.slackUrl} target="_blank"><img className="icon" src="../../../../imagenescoderise/slack.png" alt="Slack" width={32} height={32} /> </a>
+                                            <a href={user.gmailUrl} target="_blank"><img className="icon" src="../../../../imagenescoderise/gmail.png" alt="Gmail" width={32} height={32} /> </a>
+                                        </div>
+                                    </div>
+                                ))}
                             </div>
+                            <button className="carousel__button carousel__button--next" onClick={handleNextSlide}>
+                                &gt;
+                            </button>
                         </div>
-                    ))}
-                </div>
-                <button className="carousel__button carousel__button--next" onClick={handleNextSlide}>
-                    Next
-                </button>
-            </div>
-        </section>
+                    </div>
+                </div >
+            </section >
+        </DashboardLayout>
     );
 };
 
