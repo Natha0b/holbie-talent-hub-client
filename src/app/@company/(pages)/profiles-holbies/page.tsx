4b6cd553d292f035412427548a1ea1b604a1800e
Natha0b <laura.barona04@gmail.com>
2023-06-15 18:51:30 -0500

    📝  update files \(add changes in the directory find\)

--- src/app/@company/(pages)/profiles-holbies/page.tsx
+++ src/app/@company/(pages)/profiles-holbies/page.tsx
@@ -1,116 +1,5 @@
 "use client";
-import React from 'react';
-import { useState } from "react";
-import './profiles.scss';
-import DashboardLayout from '../find/layout';
-import Image from 'next/image';
-import javascriptIcon from '../../../../../public/imagenescoderise/java-script1.png';
-import cIcon from '../../../../../public/imagenescoderise/c-.png';
-import rubyIcon from '../../../../../public/imagenescoderise/ruby.png';
-import { ProfileSection } from "$/app/@company/components/profile/ProfileSection/ProfileSection";
-import { Carousel } from "$/app/@company/components/profile/Carousel/Carousel";
-import { ProfileCard } from "$/app/@company/components/profile/ProfileCard/ProfileCard";
-import { UserInfo } from "$/app/@company/components/profile/UserInfo/UserInfo";
-import { ContactIcons } from "$/app/@company/components/profile/ContactIcons/ContactIcons";
-
-export interface User {
-    name: string;
-    profilePicture: string;
-    age: number;
-    programmingLanguages: string[];
-    githubUrl: string;
-    linkedinUrl: string;
-    gmailUrl: string;
-    slackUrl: string;
-
-}
-
-export default function HomeProfiles() {
-    const users: User[] = [
-        {
-            name: 'John Doe',
-            profilePicture: 'profile1.jpg',
-            age: 30,
-            programmingLanguages: ['JavaScript', 'C'],
-            githubUrl: 'https://github.com/',
-            linkedinUrl: 'https://linkedin.com/',
-            gmailUrl: 'https://gmail.com',
-            slackUrl: 'https://slack.com/',
-        },
-        {
-            name: 'Ana diaz',
-            profilePicture: 'profile2.jpg',
-            age: 25,
-            programmingLanguages: ['JavaScript', 'Ruby'],
-            githubUrl: 'https://github.com/',
-            linkedinUrl: 'https://linkedin.com/',
-            gmailUrl: 'https://gmail.com',
-            slackUrl: 'https://slack.com/',
-        },
-        {
-            name: 'Jane Smith',
-            profilePicture: 'profile3.jpg',
-            age: 25,
-            programmingLanguages: ['JavaScript', 'C', 'Ruby'],
-            githubUrl: 'https://github.com/',
-            linkedinUrl: 'https://linkedin.com/',
-            gmailUrl: 'https://gmail.com',
-            slackUrl: 'https://slack.com/',
-        },
-        // Agrega más usuarios aquí si es necesario
-    ];
-
-    const [currentSlide, setCurrentSlide] = useState(0);
-
-    /*Estas funciones actualizan el estado currentSlide para mostrar el perfil de usuario anterior o siguiente en el carrusel.*/
-    const handlePrevSlide = () => {
-        setCurrentSlide((prevSlide) => (prevSlide === 0 ? users.length - 1 : prevSlide - 1));
-    };
-
-    const handleNextSlide = () => {
-        setCurrentSlide((prevSlide) => (prevSlide === users.length - 1 ? 0 : prevSlide + 1));
-    };
-
-    const getLanguageIcon = (language: string) => {
-        switch (language) {
-            case 'JavaScript':
-                return <Image src={javascriptIcon} alt="JavaScript" width={32} height={32} />;
-            case 'C':
-                return <Image src={cIcon} alt="C" width={32} height={32} />;
-            case 'Ruby':
-                return <Image src={rubyIcon} alt="Ruby" width={32} height={32} />;
-            default:
-                return null;
-        }
-    };
-
-
-    return (
-        <>
-            <Carousel users={users} handlePrevSlide={function (): void {
-                throw new Error('Function not implemented.');
-            } } handleNextSlide={function (): void {
-                throw new Error('Function not implemented.');
-            } } currentSlide={0}/>
-            {/* <ContactIcons user={undefined}/>
-            <ProfileCard user={undefined} index={0} currentSlide={0}/> */}
-            <ProfileSection users={[]} handlePrevSlide={function (): void {
-                throw new Error('Function not implemented.');
-            } } handleNextSlide={function (): void {
-                throw new Error('Function not implemented.');
-            } } currentSlide={0}/>
-            {/* <UserInfo user={undefined}/> */}
-            <ProfileSection
-                users={users}
-                handlePrevSlide={handlePrevSlide}
-                handleNextSlide={handleNextSlide}
-                currentSlide={currentSlide}
-            />
-        </>
-    );
-};
-
-/*"use client";
+"use client";
 import React from 'react';
 import { useState } from "react";
 import './profiles.scss';
@@ -169,7 +58,7 @@ const HomeProfiles: React.FC = () => {
 
     const [currentSlide, setCurrentSlide] = useState(0);
 
-    Estas funciones actualizan el estado currentSlide para mostrar el perfil de usuario anterior o siguiente en el carrusel.
+    //Estas funciones actualizan el estado currentSlide para mostrar el perfil de usuario anterior o siguiente en el carrusel.
     const handlePrevSlide = () => {
         setCurrentSlide((prevSlide) => (prevSlide === 0 ? users.length - 1 : prevSlide - 1));
     };
@@ -253,4 +142,17 @@ const HomeProfiles: React.FC = () => {
     );
 };
 
-export default HomeProfiles;*/
+export default HomeProfiles;
+
+
+/*"use client";
+import React from 'react';
+import { useState } from "react";
+import './profiles.scss';
+import DashboardLayout from '../find/layout';
+import Image from 'next/image';
+import javascriptIcon from '../../../../../public/imagenescoderise/java-script1.png';
+import cIcon from '../../../../../public/imagenescoderise/c-.png';
+import rubyIcon from '../../../../../public/imagenescoderise/ruby.png';
+
+*/
