8f5612b15c9e59ac8791a927948c79abbff538f6
Natha0b <laura.barona04@gmail.com>
2023-06-16 15:24:57 -0500

    📝  update files \(add changes in the directory find\)

--- src/app/@company/(pages)/find/layout.tsx
+++ src/app/@company/(pages)/find/layout.tsx
@@ -5,37 +5,42 @@ import Logo from '../../../../../public/logo.png'
 import { getSegment } from '$/app/share/router/router.share';
 
 export default function DashboardLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
 
-  console.log('children', children);
-  const segment = getSegment(children);
+    console.log('children', children);
+    const segment = getSegment(children);
 
-  return (
-    <>
-      <header className={styles["header"]}>
-        <figure className={styles["header__company-icon"]}>
-        </figure>
-        <nav className={styles["navigation"]}>
-          <div
-            className={`${styles["navigation__buttons"]} ${segment.includes('profile') ? styles["navigation__buttons--active"] : ''}`}
-          >
-            <Link href="/find/profile" >select your profile</Link>
-          </div>
-          <div
-            className={`${styles["navigation__buttons"]} ${segment.includes('other-one') ? styles["navigation__buttons--active"] : ''}`}
-          >
-            <Link href="/find/other-one" >custom your profile</Link>
-          </div>
-        </nav>
-        <figure className={styles["header__profile-icon"]}>
-        </figure>
-      </header>
-      <main>
-        {children}
-      </main>
-    </>
-  );
+    return (
+        <>
+            <header className={styles["header"]}>
+                <figure className={styles["header__company-icon"]}>
+                    <img src="https://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_630,w_1200,f_auto,q_auto/79001/331125_630361.png"
+                    />
+                </figure>
+                <nav className={styles["navigation"]}>
+                    <div
+                        className={`${styles["navigation__buttons"]} ${segment.includes('profile') ? styles["navigation__buttons--active"] : ''}`}
+                    >
+                        <Link href="/find/profile" >select your profile</Link>
+                    </div>
+                    <div
+                        className={`${styles["navigation__buttons"]} ${segment.includes('other-one') ? styles["navigation__buttons--active"] : ''}`}
+                    >
+                        <Link href="/find/other-one" >custom your profile</Link>
+                    </div>
+                </nav>
+                <figure className={styles["header__profile-icon"]}>
+                </figure>
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer className={styles["footer"]}>
+                <center><img src="https://apply.holbertonschool.com/holberton-logo.png" alt="Image 1" /></center>
+            </footer>
+        </>
+    );
 }
--- src/app/@company/(pages)/profiles-holbies/page.tsx
+++ src/app/@company/(pages)/profiles-holbies/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-"use client";
 import React from 'react';
 import { useState } from "react";
 import './profiles.scss';
@@ -9,7 +8,7 @@ import javascriptIcon from '../../../../../public/imagenescoderise/java-script1.
 import cIcon from '../../../../../public/imagenescoderise/c-.png';
 import rubyIcon from '../../../../../public/imagenescoderise/ruby.png';
 
-interface User {
+export interface User {
     name: string;
     profilePicture: string;
     age: number;
@@ -21,7 +20,7 @@ interface User {
 
 }
 
-const HomeProfiles: React.FC = () => {
+export default function HomeProfiles() {
     const users: User[] = [
         {
             name: 'John Doe',
@@ -47,7 +46,7 @@ const HomeProfiles: React.FC = () => {
             name: 'Jane Smith',
             profilePicture: 'profile3.jpg',
             age: 25,
-            programmingLanguages:['JavaScript', 'C', 'Ruby'],
+            programmingLanguages: ['JavaScript', 'C', 'Ruby'],
             githubUrl: 'https://github.com/',
             linkedinUrl: 'https://linkedin.com/',
             gmailUrl: 'https://gmail.com',
@@ -58,7 +57,7 @@ const HomeProfiles: React.FC = () => {
 
     const [currentSlide, setCurrentSlide] = useState(0);
 
-    //Estas funciones actualizan el estado currentSlide para mostrar el perfil de usuario anterior o siguiente en el carrusel.
+    /*Estas funciones actualizan el estado currentSlide para mostrar el perfil de usuario anterior o siguiente en el carrusel.*/
     const handlePrevSlide = () => {
         setCurrentSlide((prevSlide) => (prevSlide === 0 ? users.length - 1 : prevSlide - 1));
     };
@@ -79,80 +78,30 @@ const HomeProfiles: React.FC = () => {
                 return null;
         }
     };
-    
 
     return (
-        <DashboardLayout>
-            <section className="profile">
-                <h1 className="profile__title">Profiles</h1>
-                <div className="container">
-                    <div className="card">
-                        <div className="carousel">
-                            <button className="carousel__button carousel__button--prev" onClick={handlePrevSlide}>
-                                &lt;
-                            </button>
-                            <div className="carousel__slide-container">
-                                {users.map((user, index) => (
-                                    <div
-                                        className={`carousel__slide ${index === currentSlide ? 'carousel__slide--active' : ''}`}
-                                        key={index}
-                                    >
-
-                                        <div className="user-info">
-                                            <div className="icon-profile">
-                                                <img className="profile-picture" src={`../../../../imagenescoderise/${user.profilePicture}`} />
-                                            </div>
-                                            <div className="user-details">
-                                                <h1 className="name">{user.name}</h1>
-                                                <p className="title">Software Engineer</p>
-                                                <p className="description">
-                                                    Industry experience: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget
-                                                    condimentum tortor, vitae gravida ipsum. Nullam ut mi vel nisi auctor fermentum. In hac habitasse
-                                                    platea dictumst.
-                                                </p>
-                                                <p className="description">Programming Languages</p>
-                                                <div className="programming-languages">
-                                                    {user.programmingLanguages.map((language, index) => (
-                                                        <div className="icons-languajes" key={index}>{getLanguageIcon(language)}</div>
-                                                    ))}
-                                                </div>
-                                                <p className="description">
-                                                    Type of work sought: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu diam eget
-                                                    dui euismod commodo vitae eget augue. Mauris vitae vestibulum quam.
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div className="contact-icons">
-                                            <a href={user.githubUrl} target="_blank"><img className="icon" src="../../../../imagenescoderise/github.png" alt="GitHub" width={32} height={32} /> </a>
-                                            <a href={user.linkedinUrl} target="_blank"><img className="icon" src="../../../../imagenescoderise/linkedin.png" alt="LinkedIn" width={32} height={32} /> </a>
-                                            <a href={user.slackUrl} target="_blank"><img className="icon" src="../../../../imagenescoderise/slack.png" alt="Slack" width={32} height={32} /> </a>
-                                            <a href={user.gmailUrl} target="_blank"><img className="icon" src="../../../../imagenescoderise/gmail.png" alt="Gmail" width={32} height={32} /> </a>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                            <button className="carousel__button carousel__button--next" onClick={handleNextSlide}>
-                                &gt;
-                            </button>
-                        </div>
-                    </div>
-                </div >
-            </section >
-        </DashboardLayout>
+        <>
+            <Carousel users={users} handlePrevSlide={function (): void {
+                throw new Error('Function not implemented.');
+            } } handleNextSlide={function (): void {
+                throw new Error('Function not implemented.');
+            } } currentSlide={0}/>
+            {/* <ContactIcons user={undefined}/>
+            <ProfileCard user={undefined} index={0} currentSlide={0}/> */}
+            <ProfileSection users={[]} handlePrevSlide={function (): void {
+                throw new Error('Function not implemented.');
+            } } handleNextSlide={function (): void {
+                throw new Error('Function not implemented.');
+            } } currentSlide={0}/>
+            {/* <UserInfo user={undefined}/> */}
+            <ProfileSection
+                users={users}
+                handlePrevSlide={handlePrevSlide}
+                handleNextSlide={handleNextSlide}
+                currentSlide={currentSlide}
+            />
+        </>
     );
 };
 
 export default HomeProfiles;
-
-
-/*"use client";
-import React from 'react';
-import { useState } from "react";
-import './profiles.scss';
-import DashboardLayout from '../find/layout';
-import Image from 'next/image';
-import javascriptIcon from '../../../../../public/imagenescoderise/java-script1.png';
-import cIcon from '../../../../../public/imagenescoderise/c-.png';
-import rubyIcon from '../../../../../public/imagenescoderise/ruby.png';
-
-*/
