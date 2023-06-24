f3bfe4b6602e9d00e7db4c790bcf23b09216a5a6
JuanDAC <juanavila12.0308@gmail.com>
2023-06-24 14:24:53 -0500

    [:recycle:] | refactor | (refactor structure of url)

--- src/app/@company/(pages)/find/projectsdetaills/page.tsx
+++ src/app/@company/(pages)/find/projectsdetaills/page.tsx
@@ -4,7 +4,8 @@ import styles from './ProjectsDetails.module.scss'
 // Import other components used within TalentSearch
 
 import { TalentPreview, Talent } from '$company/components/TalentPreview/TalentPreview';
-import { Carousel, handlePrevSlide, handleNextSlide, currentSlide }from '$company/components/profile/Carousel';
+// import { Carousel, handlePrevSlide, handleNextSlide, currentSlide }from '$company/components/profile/Carousel';
+
 
 
 interface Profile {
@@ -59,68 +60,7 @@ export interface Skill {
     updated_at: string;
 }
 
-const UserProfile: React.FC<{ profile: Profile }> = ({ profile }) => {
-    return (
-        <div className={styles.profile}>
-            <div className={styles.details_profile}>
 
-                {projectsData.map((project) => (
-                    <div key={project.project_id}>
-                        <h1>{project.title}</h1>
-                        <h2>{project.description}</h2>
-                        <h3>The Holberton-WeatherApp displays the current and detailed weather forecast for the specified location, including temperature, humidity, wind speed, and an overall weather description📑.</h3>
-                        <h3>In addition to the current forecast, the application also provides the weather forecast for the next 5 days, allowing users to plan their activities accordingly☔.</h3>
-                        <h3>The Holberton-WeatherApp runs from the command line, enabling users to quickly obtain weather information without the need for a complex graphical interface.</h3>
-                        <h3>Users can customize the temperature unit (Celsius or Fahrenheit) and default location in the application settings, allowing them to tailor the experience to their preferences.</h3>
-                        <p>Repository: <a href={project.repository}>{project.repository}</a></p>
-                        <p>Website: <a href={project.website}>{project.website}</a></p>
-                        <p>Start Date: {project.start_date}</p>
-                        <p>End Date: {project.end_date}</p>
-                        <p>Updated At: {project.updated_at}</p>
-                    </div>
-                ))}
-
-                {skillsData.map((skill) => (
-                    <div key={skill.skill_id}>
-                        <h2>{skill.name}</h2>
-                        <h3>{skill.description}</h3>
-                    </div>
-                ))}
-
-                <div>
-                    <h3>Installation and Usage Instructions</h3>
-                    <li>git clone https://github.com/your_username/Holberton-WeatherApp.git</li>
-                    <p>Install the necessary dependencies by running the following command:</p>
-                    <li>pip install -r requirements.txt</li>
-                    <p>Retrieve the weather forecast for a specific location:</p>
-                    <li>python app.py --location "Medellín"</li>
-                </div>
-            </div>
-            <div className={styles.holbies_parnets}>
-                <div className={styles.resultsContainer}>
-                    {
-                        fakeData.map((talent) => (
-                            <TalentPreview talent={talent} />
-                        ))
-                    }
-                    {/* Render talent previews based on search results */}
-                    {/* Use the TalentPreview component */}
-                </div>
-
-
-            </div>
-
-            <div className={styles.info_multimedia}>
-                    {multimedia.map((media) => (
-                        <div key={media.media_id}>
-                            <span>{media.media_type}</span>
-                            <img src={media.file_path} alt="Project Media" />
-                        </div>
-                    ))}
-            </div>
-        </div>
-    );
-};
 
 // Ejemplo de información falsa
 const profileData: Profile = {
@@ -165,23 +105,45 @@ const skillsData: Skill[] = [
 
 const fakeData: Talent[] = [
     {
-        profilePicture: 'https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        name: 'Ali',
-        title: 'Web Developer',
-        bio: 'Soy apasionada desarrolladora web con experiencia en el campo. Me encanta crear aplicaciones y sitios web que no solo sean visualmente atractivos, sino también funcionales y eficientes.',
+        profilePicture: 'https://avatars.githubusercontent.com/u/40?v=4',
+        name: 'Liam Walker',
+        title: 'Frontend Developer',
+        bio: 'Passionate frontend developer with expertise in building responsive and interactive web applications. Proficient in HTML, CSS, and JavaScript frameworks such as React and Vue.js. Committed to creating engaging user experiences and writing clean, maintainable code.',
+        githubLink: 'https://github.com/liamwalker',
+        linkedinLink: 'https://www.linkedin.com/in/liamwalker',
+        slackLink: '@Liam Walker',
+        gmailLink: 'liamwalker@gmail.com',
     },
     {
-        profilePicture: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        name: 'Andres',
-        title: 'Web Developer',
-        bio: 'Tengo un sólido conocimiento de HTML, CSS y JavaScript, y me encanta trabajar con frameworks como React y Angular para desarrollar interfaces interactivas y dinámicas.',
+        profilePicture: 'https://avatars.githubusercontent.com/u/41?v=4',
+        name: 'Aurora Turner',
+        title: 'Data Scientist',
+        bio: 'Data scientist with expertise in data analysis, machine learning, and statistical modeling. Proficient in Python, R, and SQL. Experienced in working with large datasets and extracting meaningful insights. Passionate about leveraging data to drive informed decision-making.',
+        githubLink: 'https://github.com/auroraturner',
+        linkedinLink: 'https://www.linkedin.com/in/auroraturner',
+        slackLink: '@Aurora Turner',
+        gmailLink: 'auroraturner@gmail.com',
     },
     {
-        profilePicture: 'https://images.pexels.com/photos/997489/pexels-photo-997489.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        name: 'Sebastian',
-        title: 'Web Developer',
-        bio: '  Tengo experiencia en la integración de API y en el desarrollo de soluciones de comercio electrónico. Siempre me mantengo actualizado/a sobre las últimas tendencias y tecnologías en el desarrollo web para ofrecer las mejores soluciones a mis clientes.',
-    }
+        profilePicture: 'https://avatars.githubusercontent.com/u/42?v=4',
+        name: 'Luna Baker',
+        title: 'UI/UX Designer',
+        bio: 'Creative UI/UX designer with a focus on crafting visually appealing and user-friendly interfaces. Proficient in design tools such as Sketch, Figma, and Adobe XD. Experienced in conducting user research, creating wireframes, and prototyping interactive designs.',
+        githubLink: 'https://github.com/lunabaker',
+        linkedinLink: 'https://www.linkedin.com/in/lunabaker',
+        slackLink: '@Luna Baker',
+        gmailLink: 'lunabaker@gmail.com',
+    },
+    {
+        profilePicture: 'https://avatars.githubusercontent.com/u/43?v=4',
+        name: 'James Wilson',
+        title: 'DevOps Engineer',
+        bio: 'DevOps engineer with expertise in automating and optimizing software development processes. Skilled in cloud platforms like AWS and Azure. Experienced in CI/CD pipelines, infrastructure as code, and containerization. Passionate about improving efficiency and collaboration in software development.',
+        githubLink: 'https://github.com/jameswilson',
+        linkedinLink: 'https://www.linkedin.com/in/jameswilson',
+        slackLink: '@James Wilson',
+        gmailLink: 'jameswilson@gmail.com',
+    },
 ]
 
 const multimedia: Multimedia[] = [
@@ -201,12 +163,65 @@ const multimedia: Multimedia[] = [
     }
 ];
 
-const App: React.FC = () => {
+
+
+const UserProfile: React.FC<{ profile: Profile }> = ({ profile }) => {
     return (
-        <div>
-            <UserProfile profile={profileData} />
+        <div className={styles.profile}>
+            <div className={styles.details_profile}>
+
+                {projectsData.map((project) => (
+                    <div key={project.project_id}>
+                        <h1>{project.title}</h1>
+                        <h2>{project.description}</h2>
+                        <h3>The Holberton-WeatherApp displays the current and detailed weather forecast for the specified location, including temperature, humidity, wind speed, and an overall weather description📑.</h3>
+                        <h3>In addition to the current forecast, the application also provides the weather forecast for the next 5 days, allowing users to plan their activities accordingly☔.</h3>
+                        <h3>The Holberton-WeatherApp runs from the command line, enabling users to quickly obtain weather information without the need for a complex graphical interface.</h3>
+                        <h3>Users can customize the temperature unit (Celsius or Fahrenheit) and default location in the application settings, allowing them to tailor the experience to their preferences.</h3>
+                        <p>Repository: <a href={project.repository}>{project.repository}</a></p>
+                        <p>Website: <a href={project.website}>{project.website}</a></p>
+                        <p>Start Date: {project.start_date}</p>
+                        <p>End Date: {project.end_date}</p>
+                        <p>Updated At: {project.updated_at}</p>
+                    </div>
+                ))}
+
+                {skillsData.map((skill) => (
+                    <div key={skill.skill_id}>
+                        <h2>{skill.name}</h2>
+                        <h3>{skill.description}</h3>
+                    </div>
+                ))}
+
+                <div>
+                    <h3>Installation and Usage Instructions</h3>
+                    <li>git clone https://github.com/your_username/Holberton-WeatherApp.git</li>
+                    <p>Install the necessary dependencies by running the following command:</p>
+                    <li>pip install -r requirements.txt</li>
+                    <p>Retrieve the weather forecast for a specific location:</p>
+                    <li>python app.py --location "Medellín"</li>
+                </div>
+            </div>
+            <div className={styles.holbies_parnets}>
+                <div className={styles.resultsContainer}>
+                    {
+                        fakeData.map((talent) => (
+                            <TalentPreview talent={talent} />
+                        ))
+                    }
+                </div>
+
+
+            </div>
+
+            <div className={styles.info_multimedia}>
+                {multimedia.map((media) => (
+                    <div key={media.media_id}>
+                        <span>{media.media_type}</span>
+                        <img src={media.file_path} alt="Project Media" />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
-
-export default App;
